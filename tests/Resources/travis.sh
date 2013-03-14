39d30ed69d92422e6fe8b3b45e30763df7a6ddec
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-03-14 01:59:13 +0100

    installer fixes

--- tests/Resources/travis.sh
+++ tests/Resources/travis.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 sudo apt-get install libgearman-dev
-printf "\n"| pecl install gearman
+wget http://pecl.php.net/get/gearman-1.1.1.tgz 
+tar -xzf gearman-1.1.1.tgz
+sh -c "cd gearman-1.1.1 && phpize && ./configure && sudo make install"
+echo "extension=gearman.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
 printf "\n"| pecl install pecl_http
