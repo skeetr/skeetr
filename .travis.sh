cabb2220deb83e6f8a60a8b89a7f8369214ef477
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-03-14 02:08:48 +0100

    travis gearman change

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get install libgearman-dev
-wget http://pecl.php.net/get/gearman-1.1.1.tgz 
-tar -xzf gearman-1.1.1.tgz
-sh -c "cd gearman-1.1.1 && phpize && ./configure && sudo make install"
-echo "extension=gearman.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-
 printf "\n"| pecl install pecl_http
+
+sudo apt-get install libgearman-dev
+wget http://pecl.php.net/get/gearman-1.0.2.tgz
+tar -xzf gearman-1.0.2.tgz
+sh -c "cd gearman-1.0.2 && phpize && ./configure && sudo make install"
+echo "extension=gearman.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
