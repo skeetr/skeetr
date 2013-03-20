6f091bf93d1ab3d76672a0bca89a02b8f93a5f8b
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-03-20 01:34:46 +0100

    added php-skeetr extension

--- .travis.sh
+++ .travis.sh
@@ -6,3 +6,9 @@ wget http://pecl.php.net/get/gearman-1.0.2.tgz
 tar -xzf gearman-1.0.2.tgz
 sh -c "cd gearman-1.0.2 && phpize && ./configure && sudo make install"
 echo "extension=gearman.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
+wget https://github.com/skeetr/php-skeetr/archive/master.zip
+unzip master
+sh -c "cd php-skeetr-master && phpize && ./configure && sudo make install"
+echo "extension=skeetr.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
